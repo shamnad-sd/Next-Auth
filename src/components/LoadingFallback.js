7cb7753650016614dfa33117e85320598a06bdce
upturnist <shamnad2nk@gmail.com>
2025-07-03 12:38:57 +0530

    first commit

---
+++ src/components/LoadingFallback.js
@@ -0,0 +1,12 @@
+function LoadingFallback() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="text-center">
+        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-indigo-600 mx-auto"></div>
+        <p className="mt-2 text-gray-600">Loading...</p>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingFallback;
